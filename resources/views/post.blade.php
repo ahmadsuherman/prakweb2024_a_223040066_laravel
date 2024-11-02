516e1e94ab4b98d16b8d54eb6598895efaeb29a4
ahmadsuherman <suhermana274@gmail.com>
2024-11-02 17:08:28 +0700

    [Video 16] Belajar Laravel 11 | 16. Searching

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,7 +8,7 @@
                     <a href="/posts" class="font-medium text-xs text-blue-600 hover:underline">&laquo; Back to all posts</a>
                     <address class="flex items-center my-6 not-italic">
                         <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                            <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="{{ $post->author->name }}">
+                            <img class="mr-4 w-16 h-16 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="{{ $post->author->name }}">
                             <div>
                                 <a href="/posts?author={{ $post->author->username }}" rel="author" class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
                                 <p class="text-base text-gray-500 dark:text-gray-400 mb-2">{{ $post->created_at->diffForHumans() }}</p>
