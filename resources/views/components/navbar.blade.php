98ad58d0a7b42d2cd0a825b1e860b86cb95b67f4
ahmadsuherman <suhermana274@gmail.com>
2024-11-03 06:39:21 +0700

    [Video 16] Belajar Laravel 8 | 16. User Login & Middleware

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -41,9 +41,11 @@
                 x-transition:leave-end="opacity-0 scale-95"
                 class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                 <!-- Active: "bg-gray-100", Not Active: "" -->
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                <a href="/dashboard" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Dashboard</a>
+                <form action="/logout" method="POST">
+                    @csrf
+                    <button class="block px-4 py-2 text-sm text-gray-700"> Sign Out</button>
+                  </form>
                 </div>
             </div>
             @endguest
@@ -84,17 +86,19 @@
         <div class="border-t border-gray-700 pb-3 pt-4">
         <div class="flex items-center px-5">
             <div class="flex-shrink-0">
-            <img class="h-10 w-10 rounded-full" src="{{  asset('img/logo.jpg') }}" alt="">
+            <img class="h-10 w-10 rounded-full" src="{{  asset('img/logo.jpg') }}" alt="Logo">
             </div>
             <div class="ml-3">
-            <div class="text-base font-medium leading-none text-white">Ahmad Suherman</div>
-            <div class="text-sm font-medium leading-none text-gray-400">suhermana274@gmail.com</div>
+            <div class="text-base font-medium leading-none text-white">{{ Auth()->user()->name }}</div>
+            <div class="text-sm font-medium leading-none text-gray-400">{{ Auth()->user()->email }}</div>
             </div>
         </div>
         <div class="mt-3 space-y-1 px-2">
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
+            <a href="/dashboard" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Dashboard</a>
+            <form action="/logout" method="POST">
+                @csrf
+                <button class="block px-4 py-2 text-sm text-gray-700"> Sign Out</button>
+              </form>
         </div>
         @endguest
         </div>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,24 +12,39 @@
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     @if (session()->has('success'))
                     <div class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
-                        <span class="sr-only">Info</span>
                         <div>
                             {{ session('success') }}
                         </div>
                     </div>
                     @endif
+
+                    @if (session()->has('error'))
+                    <div class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+                        <div>
+                            {{ session('error') }}
+                        </div>
+                    </div>
+                    @endif
+
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Sign in to your account
                     </h1>
 
-                    <form class="space-y-4 md:space-y-6" action="#">
+                    <form class="space-y-4 md:space-y-6" action="/login" method="POST">
+                        @csrf
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white @error('email') text-red-700 dark:text-red-500 @enderror">Email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('email') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500 @enderror" placeholder="name@gmail.com" required="" value="{{ old('email') }}">
+                            @error('email')
+                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                            @enderror
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white @error('password') text-red-700 dark:text-red-500 @enderror">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 @error('password') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500 @enderror" required="">
+                            @error('password')
+                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                            @enderror
                         </div>
                         <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
